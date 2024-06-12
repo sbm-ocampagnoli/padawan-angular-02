d9556d5e9b6c5bb5bee92f4903c37e27ccdcbc11
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-12 10:13:45 -0300

    Adicionando validações customizadas.

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -2,6 +2,7 @@ import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -21,7 +22,7 @@ export class NovoUsuarioComponent implements OnInit {
     this.novoUsuarioForm = this.formBuilder.group({
       email: ['', [Validators.email, Validators.required]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
-      userName: [''],
+      userName: ['', [minusculoValidator]],
       password: [''],
     });
   }
